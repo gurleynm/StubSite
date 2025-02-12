dcce367cec70925469db0cff2f8fb32e482876c5
Nathan Gurley <44032733+gurleynm@users.noreply.github.com>
2025-02-12 12:46:58 -0500

    Easy color customization and a footer

--- Constants.cs
+++ Constants.cs
@@ -3,15 +3,26 @@
     public class Constants
     {
         public static Dictionary<string, string> ColorScheme = new Dictionary<string, string> {
-            { "Main", "#f4b933" }
-            ,{ "Accent", "#063558" }
+            { "Main", "#f9da56" }
+            ,{ "Accent", "#fd8748" }
             ,{ "Text", "black" }
         };
 
         public static bool Mobile { get; set; }
         public static EventHandler<string> CurPageChanged = (sender, value) => { };
         public static string CompanyName = "Sitwell's Coffee House Act II";
-        public static string CompanyContact = "Info.Sitwells@gmail.com";
+        public static string CompanyEmail = "Info.Sitwells@gmail.com";
+        public static string CompanyPhone = "(513) 281-7487";
+        public static string CompanyAddress = "324 Ludlow Ave, Cincinnati, OH 45220";
+        public static Dictionary<string,string> CompanyHours = new() { 
+            { "Monday" ,"8AM-9PM" } 
+            ,{ "Tuesday" ,"8AM-9PM" } 
+            ,{ "Wednesday" ,"8AM-9PM" } 
+            ,{ "Thursday" ,"8AM-9PM" } 
+            ,{ "Friday" ,"8AM-9PM" } 
+            ,{ "Saturday" ,"9AM-9PM" } 
+            ,{ "Sunday" ,"9AM-5PM" } 
+        };
 
         private static string curPage;
         public static string CurPage

dbe5a9ad1303d86b626eb0cbefd1b66549544a04
Nathan Gurley <44032733+gurleynm@users.noreply.github.com>
2026-06-15 11:59:22 -0400

    Visuals

--- Constants.cs
+++ Constants.cs
@@ -14,22 +14,14 @@
         public static string CompanyName = "Bersani Yoga";
         public static string CompanyEmail = "annabersani@gmail.com";
         public static string CompanyPhone = "(513) 683-4611";
-        public static string CompanyAddress = "174 Karl Brown Way Loveland, OH 45140";
-        public static string CompanyMapsURL = "https://maps.app.goo.gl/uJGaX9ShQaHHAie36";
+        public static string CompanyAddress = "Locations vary";
+        public static string CompanyMapsURL = ""; //"https://maps.app.goo.gl/uJGaX9ShQaHHAie36";
+        
         public static Dictionary<string,string> CompanyHours = new() { 
-            
+            { "Hours" ,"Times vary" } 
         };
-        //public static Dictionary<string,string> CompanyHours = new() { 
-        //    { "Monday" ,"7:30AM-6:00PM" } 
-        //    ,{ "Tuesday" ,"7:30AM-6:00PM" } 
-        //    ,{ "Wednesday" ,"7:30AM-6:00PM" } 
-        //    ,{ "Thursday" ,"7:30AM-6:00PM" } 
-        //    ,{ "Friday" ,"7:30AM-6:00PM" } 
-        //    ,{ "Saturday" ,"8:00AM-6:00PM" } 
-        //    ,{ "Sunday" ,"12:00PM-6:00PM" } 
-        //};
         public static Dictionary<string,string> CompanySocials = new() { 
-            //{ "Instagram" ,"https://www.instagram.com/loveland_yoga" } 
+            { "Instagram" ,"https://www.instagram.com/aebersani" } 
             //,{ "Facebook" ,"https://www.facebook.com/LovelandYoga" }
             //,{ "X" ,"https://x.com/lovelandyoga" }
         };

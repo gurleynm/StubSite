9b5c0d402facf200c6851cee4aec7b9ce0fc0d0a
Nathan Gurley <44032733+gurleynm@users.noreply.github.com>
2026-06-15 11:07:59 -0400

    Add booking stuff

---
+++ Data/Booking/BookingOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace StubSite.Data.Booking
+{
+    public class BookingOptions
+    {
+        public string PrimaryColor { get; set; } = Constants.ColorScheme["Accent"];
+
+        public string AccentColor { get; set; } = "#E8F1FB";
+
+        public bool ShowAvailabilityCount { get; set; } = true;
+
+        public bool ShowSessions { get; set; } = true;
+
+        public bool ShowSessionDescriptions { get; set; } = true;
+
+        public bool Compact { get; set; } = false;
+
+        public int DaysToShow { get; set; } = 30;
+    }
+}
